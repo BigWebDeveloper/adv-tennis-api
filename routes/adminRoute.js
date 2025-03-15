70c09b5b51b9393f0942d19db4823b22da2beb9a
Azeez Oloto <YOUR_EMAIL>
2025-03-15 02:17:41 +0100

    unintalling related flagged dependency bcrypt

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,7 +1,7 @@
 import express from "express"
-import { loginAdmin, registerAdmin } from "../controllers/adminController.js"
+// import { loginAdmin, registerAdmin } from "../controllers/adminController.js"
 
 export const adminRouter = express.Router();
 
-adminRouter.post("/register",registerAdmin)
-adminRouter.post("/login",loginAdmin)
+// adminRouter.post("/register",registerAdmin)
+// adminRouter.post("/login",loginAdmin)
